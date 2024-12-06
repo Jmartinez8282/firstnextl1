7b9946541b21f42b8367d6083f17a0530797e20d
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2024-12-05 22:34:48 -0800

    Next Auth set up, Google Cloud set up

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,12 @@
+'use client'
+import { useSession } from "next-auth/react"
 import Link from "next/link"
 
 
 const NavBar = () => {
+
+  const {status,data:session} = useSession()
+
   return (
   <>
     <div className="flex bg-slate-200 p-5">
@@ -9,6 +14,9 @@ const NavBar = () => {
         <Link href={'/'} className="mr-5">Next js</Link>
         <Link href={'/users'}className="mr-5">Users</Link>
         <Link href={'/admin'}className="mr-5">Admin</Link>
+        {status === 'loading' && <div>Loading....</div> }
+        {status === 'authenticated' && <div>{session.user?.name}</div> }
+       {status === 'unauthenticated' && <Link href={'/api/auth/signin'}className="mr-5">Login</Link>}
 
     </div>
   </>
